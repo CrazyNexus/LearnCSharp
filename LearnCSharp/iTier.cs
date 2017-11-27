d213e53a13d8dcbfa7cca30a9134abe0cd787e63
Thomas Dubiel <td@crazynexus.de>
2017-11-27 14:25:54 +0100

    add a new interface

---
+++ LearnCSharp/iTier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    interface ITier
+    {
+        // Eigenschaften
+        int Alter { set; get; }
+        string Geschlecht { set; get; }
+
+        // Methoden
+        void Essen();
+        void Trinken();
+    }
+}
